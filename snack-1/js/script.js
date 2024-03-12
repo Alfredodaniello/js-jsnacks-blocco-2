6cde2bd6054f300cd6cd5cb7ccc04664d66283ae
Alfredodaniello <alfredo_daniello@libero.it>
2024-03-12 13:39:53 +0100

    snack-1 completed

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -5,6 +5,7 @@ se è dispari stampa il numero successivo
 
 //Chiedo all'utente di inserire un numero
 const userNumber = parseInt(prompt("Inserisci un numero!"));
+//Controllo se il numero inserito sia pari e stampo il risultato corrispettivo alla consegna
 if (userNumber % 2 === 0){
     alert(userNumber);
 } else {
